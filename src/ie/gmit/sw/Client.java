1b9eaca41aaf79506588a1ca34b6f5cedca8801e
Darren Butler <Darren-B312@Outlook.com>
2020-01-07 20:46:14 +0000

    first commit

---
+++ src/ie/gmit/sw/Client.java
@@ -0,0 +1,8 @@
+package ie.gmit.sw;
+
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println("this is a client");
+	}
+}
---
+++ src/ie/gmit/sw/Server.java
@@ -0,0 +1,8 @@
+package ie.gmit.sw;
+
+public class Server {
+
+	public static void main(String[] args) {
+		System.out.println("this is a server");
+	}
+}
